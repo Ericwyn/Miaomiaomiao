7d6471cba9befc1a0f3b38e07d7c2a77ddc3c3d1
Ericwyn <Ericwyn.chen@gmail.com>
2017-07-21 15:10:21 +0800

    初次提交～写了一个测试的类看看混淆后的效果

---
+++ src/test/呜喵.java
@@ -0,0 +1,18 @@
+package test;
+
+/**
+ * Created by Ericwyn on 17-7-21.
+ */
+public class 呜喵{
+    private String 喵;
+    public 呜喵(String 喵){
+        this.喵=喵;
+    }
+    public void 喵(){
+        System.out.println(喵);
+    }
+    public String 喵喵(){
+        return this.喵;
+    }
+
+}
---
+++ src/test/喵呜.java
@@ -0,0 +1,15 @@
+package test;
+
+/**
+ * Created by Ericwyn on 17-7-21.
+ */
+public class 喵呜 {
+    public static void main(String[] args){
+
+        喵喵喵 喵喵喵=new 喵喵喵();
+        喵喵喵 喵喵=new 喵喵喵();
+        喵喵喵.喵呜();
+        喵喵.喵喵喵喵();
+        喵喵喵.喵呜(new 呜喵("test.喵喵喵").喵喵());
+    }
+}
